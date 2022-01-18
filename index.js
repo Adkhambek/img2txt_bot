534dc9c622d762863d2d28f008cdfff14a6f6f75
Adham <muzaffarov.adham@gmail.com>
2022-01-18 20:36:32 +0500

    help command

--- index.js
+++ index.js
@@ -4,12 +4,39 @@ const bot = new Telegraf(process.env.TOKEN);
 
 // Error Handling
 bot.catch((err, ctx) => {
+    console.log(err);
     return ctx.reply("Something wrong !", { parse_mode: "HTML" });
 });
 
 // Public
 bot.start((ctx) => {
-    ctx.reply("OK");
+    const firstName = ctx.message.chat.first_name;
+    const messageId = ctx.message.message_id;
+    ctx.reply(
+        `Hi! ${firstName} 😀
+
+I am an Optical Character Recognizer Bot 🤖. 
+
+Just send a clear image 🖼️ and I will recognize the text in the image and send it as a message!
+
+Get the list of commands by /help`,
+        { reply_to_message_id: messageId }
+    );
+});
+
+bot.help((ctx) => {
+    const messageId = ctx.message.message_id;
+    ctx.reply(
+        `<b>List of commands available:</b>\n        
+/start - To start the bot        
+/usage - To get your usage info     
+/help - To show this message        
+/contact - To get contact of the developer`,
+        {
+            parse_mode: "HTML",
+            reply_to_message_id: messageId,
+        }
+    );
 });
 
 module.exports = bot;
